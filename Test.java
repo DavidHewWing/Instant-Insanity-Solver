1067066d0b72aff899ad0937eee64524531ab6da
David Hew-wing <dhew67@gmail.com>
2018-03-19 17:56:06 -0400

    color works and cube works

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test{
+
+	public static void main(String args[]){
+		Cube c ;
+		c = new Cube (new Color[]{Color.BLUE,Color.GREEN,Color.WHITE,Color.GREEN,Color.BLUE,Color.RED});
+		System.out.println(c);
+	}
+
+}
