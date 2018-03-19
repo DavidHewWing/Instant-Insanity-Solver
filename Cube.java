2d8553b68be20cfccfc8b84ba56d2f99c1e59c54
David Hew-wing <dhew67@gmail.com>
2018-03-19 17:35:24 -0400

    attempt at cube.java

---
+++ Cube.java
@@ -0,0 +1,47 @@
+public class Cube{
+
+	private Color up; //index 0 
+	private Color front; //index 1
+	private Color right; //index 2
+	private Color back; //index 3
+	private Color left; //index 4
+	private Color down; //index 5
+
+
+	public Cube(Color[] faces){
+		up = faces[0];
+		front = faces[1];
+		right = faces[2];
+		back = faces[3];
+		left = faces[4];
+		down = faces[5];
+	}
+
+	public Color getUp(){
+		return up;
+	}
+
+	public Color getFront(){
+		return front;
+	}
+	
+	public Color getRight(){
+		return right;
+	}
+	
+	public Color getBack(){
+		return back;
+	}
+	
+	public Color getLeft(){
+		return left;	
+	}
+	
+	public Color getDown(){
+		return down;
+	}
+
+	public String toString(){
+		return "[" + this.up + ", " + this.front + ", " + this.right + ", "+ this.back + ", "+ this.left + ", "+ this.down + "]";
+	}
+}
