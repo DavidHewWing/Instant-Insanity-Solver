636b18dc9ec4ff5f3f23f6a471bc891b6183181e
Kyle Pinkerton <kpink777@hotmail.com>
2018-03-21 17:40:41 -0400

    cube working with test

--- Test.java
+++ Test.java
@@ -1,8 +1,36 @@
 public class Test{
 
-	public static void main(String args[]){
-		Cube c ;
-		c = new Cube (new Color[]{Color.BLUE,Color.GREEN,Color.WHITE,Color.GREEN,Color.BLUE,Color.RED});
-		System.out.println(c);
-	}
+	public static void main(String[] args){
+
+		Cube c;
+
+		c = new Cube(new Color[] {Color.BLUE, Color.GREEN, Color.WHITE, Color.GREEN, Color.BLUE, Color.RED});
+
+		c.reset();
+
+		while (c.hasNext()){
+
+			c.next();
+			System.out.println(c);
+
+
+
+		}
+
+		System.out.println("reset:");
+		c.reset();
+		while(c.hasNext()){
+
+			c.next();
+
+			System.out.println(c);
+
+		}
+
+
+		}
+
+
 }
+
+
